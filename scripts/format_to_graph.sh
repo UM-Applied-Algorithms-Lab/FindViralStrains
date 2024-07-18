3f212f8f5b06a515087b8269b562a3abeb651c4a
Kaylbing <mckaylbing@gmail.com>
2024-07-18 22:03:09 +0000

    Add the ability to graph flows in a pdf

--- scripts/format_to_graph.sh
+++ scripts/format_to_graph.sh
@@ -2,19 +2,20 @@
 #!/bin/bash
 
 # Check if the input file is provided #
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 input_file"
-  exit 1
-fi
+#if [ "$#" -ne 1 ]; then
+ # echo "Usage: $0 input_file"
+ # exit 1
+#fi
 
 # Define the input file from the command-line argument #
 input_file="$1"
+input_file2="$2"
 
 # Check if the input file exists #
-if [ ! -f "$input_file" ]; then
-  echo "Error: File '$input_file' not found!"
-  exit 1
-fi
+#if [ ! -f "$input_file" ]; then
+ # echo "Error: File '$input_file' not found!"
+  #exit 1
+#fi
 
 # Define the output DOT file and PNG file based on the input file name #
 output_dot_file="graph.dot"
@@ -37,7 +38,7 @@ while read -r line; do
 if [[ -z $target ]]; then
 	continue
 fi
- 
+
   # Convert the labels to the desired format #
   source_label="${source//+/_plus}"
   source_label="${source_label//-/_minus}"
@@ -45,8 +46,27 @@ fi
   target_label="${target_label//-/_minus}"
 
   # Print the node relationship #
-  echo "  \"$source_label\" -> \"$target_label\" [label=${weight}] ;" >> "$output_dot_file"
+  echo "  \"$source_label\" -> \"$target_label\" [label=\"${weight}\"] ;" >> "$output_dot_file"
 done < "$input_file"
+
+
+# Reads in second file all at once #
+full_path=$(awk 'NR==7' $input_file2)
+tokens=( $full_path )
+
+# Iterate over the tokens to create edges
+for num in $(seq 2 ${#tokens[@]}); do
+  # Convert the labels to the desired format
+  source="${tokens[$num-1]}"
+  target="${tokens[$num]}"
+  source_label="${source//+/_plus}"
+  source_label="${source_label//-/_minus}"
+  target_label="${target//+/_plus}"
+  target_label="${target_label//-/_minus}"
+
+  # Print the converted labels
+  echo "  \"$source_label\" -> \"$target_label\" [color=blue] ;" >> "$output_dot_file"
+done
 
 # End the DOT file #
 echo "}" >> "$output_dot_file"
