9a02bfc679544d3059221eea683303e501faef48
Kaylbing <mckaylbing@gmail.com>
2025-01-09 18:54:08 +0000

    Edit slurm hardware reqs

--- slurm_files/smk_submit.sh
+++ slurm_files/smk_submit.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 
-#SBATCH --job-name=my_job          # Job name
+#SBATCH --job-name=MFD_SMK          # Job name
 
 #SBATCH --output=job_output_%j.log # Standard output and error log
 
 #SBATCH --partition=cpu(all)       # Partition (queue) name
 
-#SBATCH --nodes=2                  # Request two nodes
+#SBATCH --nodes=1                  # Request two nodes
 
-#SBATCH --ntasks-per-node=3        # Request 3 tasks per node
+#SBATCH --ntasks-per-node=1        # Request 3 tasks per node
 
-#SBATCH --time=01:00:00            # Time limit hrs:min:sec (e.g., 1 hour)
+#SBATCH --cpus-per-task=4
 
+#SBATCH --mem=16GB
 
 # Load any necessary modules or environment settings here
 
