a2cb61c6ec3576248a643c7a57e47e7237c8f7b4
Mikhail <mckaylbing@gmail.com>
2025-05-13 15:28:58 -0600

    Delete output data, and add compress graph dir

--- libs/compress_graph/src/main.rs
+++
@@ -1,8 +0,0 @@
-// Take graph, sources, and sinks, sort each of them, then go through and find the amount
-// of connections for each node. If the total connections for a node is more than two,
-// skip to the next one, otherwise compress (no need to find all connection, just 3)
-
-
-fn main() {
-    println!("Hello, world!");
-}
