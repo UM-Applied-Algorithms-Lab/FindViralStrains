354e98a03245f40e4a13e94533b242c3f5a2a8c5
Mikhail <mckaylbing@gmail.com>
2025-05-23 14:32:14 -0600

    Remove more unneeded files

--- build.sh
+++
@@ -1,8 +0,0 @@
-echo "Compiling needed rust files..."
-cd libs/graph_analyze/src
-cargo build --release
-cd ../../..
-cd libs/super_source_and_sink/src
-cargo build --release
-cd ../../..
-echo "Build Complete."
